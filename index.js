9ee42e3e515a2bc96129ad0ac9e7b62cb1d7707c
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-10-19 15:15:15 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(friends, event) {
+  const newArray = [];
+    for (let i = 0; i < friends.length; i++) {
+        newArray[i] = (`Thank you, ${friends[i]}, for the wonderful ${event} gift!`);
+    debugger;
+    }
+    return newArray;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+
+function countDown(integer = 10){
+while (integer > -1) {
+  console.log(integer--);
+}
+return countDown;
+}
+
+
